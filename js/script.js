3ee66f4a4b3c8d1f52524e0848cff3baed05b208
jaqueshere <jaques.gillis@gmail.com>
2016-04-01 19:38:06 -0400

    Added carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 	console.log("jQuery started");
+	$(".owl-carousel").owlCarousel({
+		singleItem: true,
+		loop: false,
+		center: true,
+		dots: false,
+		navigation:true,
+		/*navigationText:[
+		"<i class='icon-chevron-left icon-white'><</i>",
+		"<i class='icon-chevron-right icon-white'>></i>"],*/
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+	$('.owl-carousel-arrows .next').click(function() {
+		owl.trigger('owl.next');
+	});
+	$('.owl-carousel-arrows .prev').click(function() {
+		owl.trigger('owl.prev');
+	});
 	$('#libai_container').click(function() {
 		$('#libai_container').css('display', 'none');
 		$('#nightshining_container').css('display', 'block');
