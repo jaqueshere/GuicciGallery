f87552ab00484c26559597a341f25ac720916015
jaqueshere <jaques.gillis@gmail.com>
2016-04-03 17:11:13 -0400

    Fixed carousel (mostly)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,36 @@
 $(document).ready(function() {
 	console.log("jQuery started");
 	$(".owl-carousel").owlCarousel({
+		items: 1,
 		singleItem: true,
 		loop: false,
 		center: true,
 		dots: false,
-		navigation:true,
-		/*navigationText:[
-		"<i class='icon-chevron-left icon-white'><</i>",
-		"<i class='icon-chevron-right icon-white'>></i>"],*/
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
-				items: 1
-			}
-		}
+		navigation:true
 	});
 	$('.owl-carousel-arrows .next').click(function() {
-		owl.trigger('owl.next');
+		$(".owl-carousel").trigger('next.owl.carousel');
 	});
 	$('.owl-carousel-arrows .prev').click(function() {
-		owl.trigger('owl.prev');
+		$(".owl-carousel").trigger('owl.carousel.prev');
 	});
+
 	$('#gallery1').css('display', 'none'); 		/* As soon as carousel width defined, remove */
 
 	$('#newPic_container').click(function() {
 		$('#newPic_container').css('display', 'none');
 		$('#portals').css('display', 'block');
 	});
+
+	$('.caption').hide();
+
+	$('.doorKnob').hover(function() {
+		$(this).next()
+			.slideToggle();
+	});
+
 	$('.one').click(function() {
 		$('#portals').css('display', 'none');
 		$('#gallery1').css('display','block');
-	})
+	});
 });
